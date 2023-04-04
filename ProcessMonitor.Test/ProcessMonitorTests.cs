6b9f33a0a3448d0f8aa3b68521509937c1a0f188
artonjakupiii <129781467+artonjakupiii@users.noreply.github.com>
2023-04-05 01:58:00 +0200

    Add files via upload

---
+++ ProcessMonitor.Test/ProcessMonitorTests.cs
@@ -0,0 +1,108 @@
+﻿using NUnit.Framework;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Assert = NUnit.Framework.Assert;
+
+namespace ProcessMonitor.Test
+{
+  [TestFixture]
+  public class ProcessMonitorTests
+  {
+    [Test]
+    public void TestProcessMonitorWithValidArgs()
+    {
+      // Arrange
+      string processName = "notepad";
+      int maxRunTime = 1;
+      int checkInterval = 1;
+
+      // Act
+      using (Process process = Process.Start(processName))
+      {
+        // Start a new task to run the ProcessMonitor program
+        Task task = Task.Run(() => ProcessMonitor.Program.Main(new string[] { processName, maxRunTime.ToString(), checkInterval.ToString() }));
+
+        // Wait before killing the program
+        System.Threading.Thread.Sleep((maxRunTime * 60 * 1000) + 5000);
+      }
+
+      // Assert
+      // The process should have been killed by the monitor
+      Process[] processes = Process.GetProcessesByName(processName);
+      Assert.AreEqual(0, processes.Length);
+    }
+
+    [Test]
+    public void TestProcessMonitorWithNoArgs()
+    {
+      // Arrange
+      string[] args = new string[] { };
+
+      // Act
+      var stringWriter = new StringWriter();
+      Console.SetOut(stringWriter);
+      ProcessMonitor.Program.Main(args);
+
+      // Assert
+      // The output should contain the usage message
+      Assert.That(stringWriter.ToString(), Does.Contain("Usage: ProcessMonitor.exe <process name> <maximum run time (minutes)> <check interval (minutes)>"));
+    }
+
+    [Test]
+    public void TestProcessMonitorWithInvalidMaxRunTimeArg()
+    {
+      // Arrange
+      string processName = "notepad";
+      string maxRunTime = "invalid";
+      int checkInterval = 1;
+
+      // Act
+      var stringWriter = new StringWriter();
+      Console.SetOut(stringWriter);
+      ProcessMonitor.Program.Main(new string[] { processName, maxRunTime, checkInterval.ToString() });
+
+      // Assert
+      // The output should contain an error message about the invalid input
+      Assert.That(stringWriter.ToString(), Does.Contain("Input string was not in a correct format."));
+    }
+
+    [Test]
+    public void TestProcessMonitorWithInvalidCheckIntervalArg()
+    {
+      // Arrange
+      string processName = "notepad";
+      int maxRunTime = 5;
+      string checkInterval = "invalid";
+
+      // Act
+      var stringWriter = new StringWriter();
+      Console.SetOut(stringWriter);
+      ProcessMonitor.Program.Main(new string[] { processName, maxRunTime.ToString(), checkInterval });
+
+      // Assert
+      // The output should contain an error message about the invalid input
+      Assert.That(stringWriter.ToString(), Does.Contain("Input string was not in a correct format."));
+    }
+
+    [Test]
+    public void TestProcessMonitorWithInvalidProcessNameArg()
+    {
+      // Arrange
+      string processName = "";
+      int maxRunTime = 5;
+      int checkInterval = 1;
+
+      // Act
+      var stringWriter = new StringWriter();
+      Console.SetOut(stringWriter);
+      ProcessMonitor.Program.Main(new string[] { processName, maxRunTime.ToString(), checkInterval.ToString() });
+
+      // Assert
+      // The output should contain an error message about the empty process name
+      Assert.That(stringWriter.ToString(), Does.Contain("The process name cannot be empty."));
+    }
+  }
+}
---
+++ ProcessMonitor.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProcessMonitor.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProcessMonitor.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("22d97676-25db-4252-a39e-f81318a4fc3e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
